13ff28bcbb3279f55656920d1da67bc619717c8b
Jesus Bocanegra <jesusbocd@gmail.comm>
2025-12-14 22:26:52 -0500

    intento de corregir el gitignore

--- plugins/local/curriculum/lang/en/local_curriculum.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string['pluginname'] = 'Curriculum';
-$string['curriculum'] = 'Curriculum';
-$string['manageplans'] = 'Manage study plans';
-$string['plugindescription'] = 
-    'Aquí podrás gestionar tus planes de estudio por nivel';
-$string['enterplugin'] = 'Entrar a panel de gestión';
